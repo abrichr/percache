c3b3ef36c2ccc1c957d22065271cda02a3525a66
Oben Sonne <obensonne@googlemail.com>
2014-03-15 20:39:26 +0100

    Update trove classifiers in setup.py

--- setup.py
+++ setup.py
@@ -9,26 +9,26 @@ with open(README) as fp:
     longdesc = fp.read()
 
 setup(name='percache',
-      version=version,
-      description='Persistently cache results of callables',
-      long_description=longdesc,
-      classifiers=[
-            'Development Status :: 4 - Beta',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.3',
-            'Topic :: Software Development',
-            'Intended Audience :: Developers'
-      ],
-      author='Oben Sonne',
-      author_email='obensonne@googlemail.com',
-      license='MIT License',
-      url='http://bitbucket.org/obensonne/percache',
-      py_modules=['percache']
-     )
+    version=version,
+    description='Persistently cache results of callables',
+    long_description=longdesc,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development',
+        'Intended Audience :: Developers'
+    ],
+    author='Oben Sonne',
+    author_email='obensonne@googlemail.com',
+    license='MIT License',
+    url='http://bitbucket.org/obensonne/percache',
+    py_modules=['percache']
+)
 
